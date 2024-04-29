e2146aaed06eba332a992a59cd9a44d9bb062852
Javi <javvigmez66@gmail.com>
2024-04-29 19:57:34 +0200

    no se que dia es ya

--- bin/kapitalbet.dart
+++ bin/kapitalbet.dart
@@ -1,30 +1,8 @@
 import 'dart:convert';
 import 'dart:io';
 import "package:http/http.dart" as http;
+import 'partido.dart';
 
-main() async {
-  obtenerpartidostemporada();
-}
-
-String? liga;
-String? temporada;
-
-obtenerpartidostemporada() async {
-  Uri url = Uri.parse("https://v2.nba.api-sports.io/games?date=2022-03-09");
-  var respuesta = await http.get(url, headers: {
-    'x-rapidapi-host': 'v2.nba.api-sports.io',
-    'x-rapidapi-key': '906c644d50b216417ceedc1f2248a1b7'
-  });
-  try {
-    if (respuesta.statusCode == 200) {
-      var partidos = json.decode(respuesta.body);
-      return partidos;
-    } else if (respuesta.statusCode == 404) {
-      throw ("Los datos no son correctos");
-    } else
-      throw ("Ha habido un error de conexión");
-  } catch (e) {
-    stdout.writeln(e);
-    main();
-  }
+void main() {
+  Partido().obtenerpartidostemporada();
 }
--- bin/partido.dart
+++ bin/partido.dart
@@ -1,43 +1,47 @@
 import "package:http/http.dart" as http;
-import 'dart:convert';
-import 'dart:io';
-class Partido {
-String? nombreLoc;
-String? nombreVis;
-String? puntosLoc;
-String? puntosVis;
-List<Partido> partidos;
 
-Partido.fromAPI(datos, estadisticas){
-  
-}
-obtenerpartidostemporada() async {
-  Uri url = Uri.parse("https://v2.nba.api-sports.io/games?date=2022-03-09");
-  var respuesta = await http.get(url, headers: {
-    'x-rapidapi-host': 'v2.nba.api-sports.io',
-    'x-rapidapi-key': '906c644d50b216417ceedc1f2248a1b7'
+import 'dart:io';
+
+import 'dart:convert';
+
+class Partido {
+  String? league;
+  String? season;
+  String? fechaInicio;
+  String? estadio;
+  String? ciudad;
+  String? pais;
+  String? equipoLocal;
+  String? equipoVisitante;
+  Map<String, dynamic> estadisticas;
+  Partido({
+    this.league,
+    this.season,
+    this.fechaInicio,
+    this.estadio,
+    this.ciudad,
+    this.pais,
+    this.equipoLocal,
+    this.equipoVisitante,
+    required this.estadisticas,
   });
-  try {
-    if (respuesta.statusCode == 200) {
-      var partidos = json.decode(respuesta.body);
-      return partidos;
-    } else if (respuesta.statusCode == 404) {
-      throw ("Los datos no son correctos");
-    } else
-      throw ("Ha habido un error de conexión");
-  } catch (e) {
-    stdout.writeln(e);
+
+  obtenerpartidostemporada() async {
+    Uri url = Uri.parse("https://v2.nba.api-sports.io/games?date=2022-03-09");
+    var respuesta = await http.get(url, headers: {
+      'x-rapidapi-host': 'v2.nba.api-sports.io',
+      'x-rapidapi-key': '906c644d50b216417ceedc1f2248a1b7'
+    });
+    try {
+      if (respuesta.statusCode == 200) {
+        var datos = json.decode(respuesta.body);
+        print(datos);
+      } else if (respuesta.statusCode == 404) {
+        throw ("Los datos no son correctos");
+      } else
+        throw ("Ha habido un error de conexión");
+    } catch (e) {
+      stdout.writeln(e);
+    }
   }
-}
-listaPartidos(){
-  partidos[];
-}
-agregarPartido(Partido partido){
-  partidos.add(partido);
-}
-mostrarPartidos(){
-  for( partido in partidos){
-    print('Local ${Partido.nombreLoc} vs Visitante ${Partido.nombreVis;}' );
-  }
-}
 }
